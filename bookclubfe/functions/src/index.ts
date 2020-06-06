4079a19437d197e092372e4e1e5c51680f186de8
Adam Miller <adam.wulfgar@gmail.com>
2020-06-06 09:08:19 -0500

    updating function to use slack secret

--- bookclubfe/functions/src/index.ts
+++ bookclubfe/functions/src/index.ts
@@ -3,18 +3,18 @@ import * as functions from 'firebase-functions';
 import {firebase_config} from '../../src/configs/firebase.config';
 
 import * as crypto from 'crypto';
-import * as tsscmp from 'tsscmp';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+ response.send("Hello from Firebase");
 });
 
 
-export const resetSurvey = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase! " + legitSlackRequest(request));
+export const resetSurveySlackTest = functions.https.onRequest((request, response) => {
+    var isLegit = legitSlackRequest(request)
+    response.send("Hello from Firebase!!! " + isLegit);
 
    });
 
@@ -22,9 +22,9 @@ export const resetSurvey = functions.https.onRequest((request, response) => {
 
 
    
-function legitSlackRequest(req) {
+function legitSlackRequest(req: any) {
     // Your signing secret
-    const slackSigningSecret = firebase_config.firebase.apiKey;
+    const slackSigningSecret = firebase_config.firebase.slackSecret;
 
     // Grab the signature and timestamp from the headers
     const requestSignature = req.headers['x-slack-signature'] as string;
@@ -39,5 +39,33 @@ function legitSlackRequest(req) {
     hmac.update(base);
 
     // Returns true if it matches
-    return tsscmp(hash, hmac.digest('hex'));
+    return timeSafeCompare(hash, hmac.digest('hex'));
 }
+
+
+function timeSafeCompare(a: any, b: any) {
+    var sa = String(a);
+    var sb = String(b);
+    var key = crypto.pseudoRandomBytes(32);
+    var ah = crypto.createHmac('sha256', key).update(sa).digest();
+    var bh = crypto.createHmac('sha256', key).update(sb).digest();
+  
+    return bufferEqual(ah, bh) && a === b;
+  }
+
+  function bufferEqual(a: any, b: any) {
+    if (a.length !== b.length) {
+      return false;
+    }
+    // `crypto.timingSafeEqual` was introduced in Node v6.6.0
+    // <https://github.com/jshttp/basic-auth/issues/39>
+    if (crypto.timingSafeEqual) {
+      return crypto.timingSafeEqual(a, b);
+    }
+    for (var i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
