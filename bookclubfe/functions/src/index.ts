9d9452052ba40e8caf157eebb6573aa0b41e7d3f
Adam Miller <adam.wulfgar@gmail.com>
2020-06-05 20:20:18 -0500

    adding some slack auth

--- bookclubfe/functions/src/index.ts
+++ bookclubfe/functions/src/index.ts
@@ -1,5 +1,10 @@
 import * as functions from 'firebase-functions';
 
+import {firebase_config} from '../../src/configs/firebase.config';
+
+import * as crypto from 'crypto';
+import * as tsscmp from 'tsscmp';
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -9,5 +14,30 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 
 
 export const resetSurvey = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+    response.send("Hello from Firebase! " + legitSlackRequest(request));
+
    });
+
+
+
+
+   
+function legitSlackRequest(req) {
+    // Your signing secret
+    const slackSigningSecret = firebase_config.firebase.apiKey;
+
+    // Grab the signature and timestamp from the headers
+    const requestSignature = req.headers['x-slack-signature'] as string;
+    const requestTimestamp = req.headers['x-slack-request-timestamp'];
+
+    // Create the HMAC
+    const hmac = crypto.createHmac('sha256', slackSigningSecret);
+
+    // Update it with the Slack Request
+    const [version, hash] = requestSignature.split('=');
+    const base = `${version}:${requestTimestamp}:${JSON.stringify(req.body)}`;
+    hmac.update(base);
+
+    // Returns true if it matches
+    return tsscmp(hash, hmac.digest('hex'));
+}
